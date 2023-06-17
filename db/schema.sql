0f741ae9af3716d9920e23834893e03a7a672bd6
vlada_caban <vladaonly@gmail.com>
2023-06-17 07:55:48 -0400

    created working schema and seeds for db

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(30), 
+    salary DECIMAL, 
+    department_id INT, 
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30),
+    role_id INT, 
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (name)
+VALUES
+    ("Production"),
+    ("Design"),
+    ("HR"), 
+    ("Legal"), 
+    ("Merchandising"), 
+    ("Sales"), 
+    ("IT"), 
+    ("Executive");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Designer", 80000, 2),
+    ("Design Director", 100000, 2),
+    ("Production Manager", 75000, 1),
+    ("Director of Production", 125000, 1),
+    ("CEO", 500000, 8),
+    ("HR Coordinator", 85000, 3), 
+    ("Paralegal", 80000, 4), 
+    ("Merchandiser", 90000, 5), 
+    ("VP of Merchandising", 150000, 5),
+    ("Sales person", 80000, 6), 
+    ("IT Support", 90000, 7);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Tom", "Johnson", 5, NULL),
+    ("Kate", "Ryan", 2, 1),
+    ("Sam", "Torres", 1, 2),
+    ("Regina", "Rodriguez", 4, 1),
+    ("Particia", "Levy", 3, 3),
+    ("Natalie", "Maynez", 9, 1),
+    ("Rebekah", "Katz", 8, 6);
